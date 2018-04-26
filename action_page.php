6d1c6d10af143bb7fabb063c9dd5dd8b2513173a
csrezadadmohammadi <35900249+csrezadadmohammadi@users.noreply.github.com>
2018-04-25 23:16:11 -0500

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,43 @@
+
+input[type=text], select, textarea {
+    width: 80%; 
+    padding: 12px; 
+    border: 0px solid #ccc; 
+    border-radius: 2px; 
+    box-sizing: border-box; 
+    margin-top: 4px; 
+    margin-bottom: 16px; 
+    resize: vertical 
+}
+
+input[type=submit] {
+    background-color: #gray;
+    color: blue;
+    padding: 14px 22px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+.container {
+    border-radius: 3px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+
+nav{
+	font-style: italic;
+	font-size: 150%;
+	float: left;
+	width: 220px; 
+	margin:0px; 
+	padding-left: 12px; 
+	padding-right: 12px;
+	padding-top: 70px;
+	padding-bottom:240%; 
+	background-color:rgba(84, 145, 180);
+} 
